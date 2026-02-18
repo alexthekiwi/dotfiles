50ccba24b266e92057dd34395221425078a45b9c
Alex Clark <hello@alexclark.co.nz>
2026-02-18 21:03:17 +1300

    fix: apply diagnostic config after LSP attaches

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,7 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.diagnostic.config({
-  virtual_text = false,
-  virtual_lines = { current_line = true },
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    vim.diagnostic.config({
+      virtual_text = false,
+      virtual_lines = { current_line = true },
+    })
+  end,
 })
