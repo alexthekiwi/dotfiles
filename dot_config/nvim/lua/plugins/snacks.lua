65c2cd8d6678cac4b9eb5eaccf036c746019419b
Alex Clark <hello@alexclark.co.nz>
2026-02-18 21:32:18 +1300

    fix: remove picker source config that was crashing nvim

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -9,17 +9,6 @@ return {
     explorer = {
       replace_netrw = true,
     },
-    picker = {
-      sources = {
-        explorer = {
-          layout = {
-            layout = {
-              position = "right",
-            },
-          },
-        },
-      },
-    },
   },
   keys = {
     { "<leader>e", function() Snacks.explorer({ layout = { layout = { position = "right" } } }) end, desc = "Explorer (right)" },
