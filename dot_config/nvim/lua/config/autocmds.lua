14e8c1a0868eba8e7a767940414a8f183d999e54
Alex Clark <hello@alexclark.co.nz>
2026-02-18 20:59:06 +1300

    chore: auto-restore nvim session on open

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-restore session when opening nvim with no file arguments
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
+  callback = function()
+    if vim.fn.argc() == 0 and not vim.g.started_with_stdin then
+      require("persistence").load()
+    end
+  end,
+  nested = true,
+})
