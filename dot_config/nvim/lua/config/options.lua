51ebd94dfade6780fdbe1b5c7b26fe234b06d5f8
Alex Clark <hello@alexclark.co.nz>
2026-02-19 15:13:27 +1300

    chore: prefer .git root detection for monorepos

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Prefer .git as root marker over package.json so monorepos resolve to the repo root
+vim.g.root_spec = { { ".git", "lua" }, "lsp", "cwd" }
+
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function()
     vim.diagnostic.config({
