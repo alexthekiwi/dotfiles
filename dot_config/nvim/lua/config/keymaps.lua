ef0f6bdec2f0be2404450224c9cadf0c9aa6354e
Alex Clark <hello@alexclark.co.nz>
2026-03-02 15:54:26 +1300

    chore: update configs

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,23 @@ vim.keymap.set("n", "<leader>yp", function()
   vim.fn.setreg("+", path)
   vim.notify(path, vim.log.levels.INFO, { title = "Copied path" })
 end, { desc = "Yank absolute path" })
+
+vim.keymap.set("n", "<leader>sc", function()
+  vim.cmd("enew")
+  vim.bo.buftype = "nofile"
+  vim.bo.bufhidden = "wipe"
+  vim.bo.swapfile = false
+end, { desc = "Scratch buffer" })
+
+vim.keymap.set("n", "<leader>sa", function()
+  if vim.fn.expand("%") == "" or vim.bo.buftype == "nofile" then
+    vim.ui.input({ prompt = "Save as: ", completion = "file" }, function(name)
+      if name and name ~= "" then
+        vim.bo.buftype = ""
+        vim.cmd("saveas " .. vim.fn.fnameescape(name))
+      end
+    end)
+  else
+    vim.cmd("write")
+  end
+end, { desc = "Save / Save as" })
