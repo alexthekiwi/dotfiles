8311a030a533e875cb19c5ab55e9e7c8122c82ad
Alex Clark <hello@alexclark.co.nz>
2026-03-04 20:15:28 +1300

    chore: add ensure_installed treesitter parsers for common filetypes

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    ensure_installed = {
+      "typescript", "tsx", "javascript", "php", "php_only",
+      "html", "css", "json", "yaml", "bash", "lua", "markdown",
+    },
     incremental_selection = {
       enable = true,
       keymaps = {
