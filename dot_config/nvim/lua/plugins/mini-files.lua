6aa1190ddf7b4a3b57c76f414bf5a0974b8432b4
Alex Clark <hello@alexclark.co.nz>
2026-03-04 20:47:42 +1300

    feat: add mini.files floating file explorer

---
+++ dot_config/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,19 @@
+return {
+  "echasnovski/mini.files",
+  keys = {
+    { "<leader>fm", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end, desc = "Mini Files (current file)" },
+    { "<leader>fM", function() require("mini.files").open(vim.uv.cwd(), true) end, desc = "Mini Files (cwd)" },
+  },
+  opts = {
+    mappings = {
+      go_in = "l",
+      go_in_plus = "L",
+      go_out = "h",
+      go_out_plus = "H",
+    },
+    windows = {
+      preview = true,
+      width_preview = 40,
+    },
+  },
+}
