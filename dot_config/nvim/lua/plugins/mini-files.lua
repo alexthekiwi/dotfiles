4596eec6a613f0bf2e02d5d8104463cfe00a474a
Alex Clark <hello@alexclark.co.nz>
2026-03-06 10:59:46 +1300

    chore: update configs

--- dot_config/nvim/lua/plugins/mini-files.lua
+++ dot_config/nvim/lua/plugins/mini-files.lua
@@ -1,5 +1,5 @@
 return {
-  "echasnovski/mini.files",
+  "nvim-mini/mini.files",
   keys = {
     { "<leader>fm", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end, desc = "Mini Files (current file)" },
     { "<leader>fM", function() require("mini.files").open(LazyVim.root(), true) end, desc = "Mini Files (root)" },
