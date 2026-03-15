4083d252cbff17e1300dcac34cb53b44ad98298a
Alex Clark <hello@alexclark.co.nz>
2026-03-16 09:25:19 +1300

    chore: update configs

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,9 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Silverstripe templates → treat as HTML
+vim.filetype.add({
+  extension = {
+    ss = "html",
+  },
+})
