c3921fa4daa7bd9d4bb7c7c4cbbbbd0d2f986f74
Alex Clark <hello@alexclark.co.nz>
2026-03-22 10:52:25 +1300

    chore: remove broken emmet-vim plugin

--- dot_config/nvim/lua/plugins/emmet.lua
+++ dot_config/nvim/lua/plugins/emmet.lua
@@ -1,42 +1,23 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        emmet_ls = {
-          filetypes = {
-            "html",
-            "css",
-            "scss",
-            "javascript",
-            "javascriptreact",
-            "typescript",
-            "typescriptreact",
-            "vue",
-            "svelte",
-            "php",
-            "blade",
-            "eruby",
-          },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      emmet_ls = {
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "svelte",
+          "php",
+          "blade",
+          "eruby",
         },
       },
-    },
-  },
-  {
-    "mattn/emmet-vim",
-    ft = {
-      "html",
-      "css",
-      "scss",
-      "javascript",
-      "javascriptreact",
-      "typescript",
-      "typescriptreact",
-      "vue",
-      "svelte",
-      "php",
-      "blade",
-      "eruby",
     },
   },
 }
