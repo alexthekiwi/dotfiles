3e0ba4ce75e33406a6ed7d77f87d1899ed8b93c0
Alex Clark <hello@alexclark.co.nz>
2026-06-24 10:43:08 +1200

    fix: mini.files fallback to cwd on unnamed buffer

--- dot_config/nvim/lua/plugins/mini-files.lua
+++ dot_config/nvim/lua/plugins/mini-files.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-mini/mini.files",
   keys = {
-    { "<leader>fm", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end, desc = "Mini Files (current file)" },
+    { "<leader>fm", function()
+      local name = vim.api.nvim_buf_get_name(0)
+      require("mini.files").open(name ~= "" and name or (vim.uv or vim.loop).cwd(), true)
+    end, desc = "Mini Files (current file)" },
     { "<leader>fM", function() require("mini.files").open(LazyVim.root(), true) end, desc = "Mini Files (root)" },
   },
   opts = {
