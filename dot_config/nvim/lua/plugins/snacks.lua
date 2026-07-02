c1d77f52292505cb2826825602f609f743be58e6
Alex Clark <hello@alexclark.co.nz>
2026-07-02 17:01:47 +1200

    feat(nvim): quieter default pickers + glob grep and hidden-file find keymaps

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -13,13 +13,13 @@ return {
     picker = {
       sources = {
         files = {
-          hidden = true,
-          ignored = true,
+          hidden = false,
+          ignored = false,
           exclude = { "node_modules", "vendor", ".git", ".next", ".nuxt", ".output", "dist", "build" },
         },
         grep = {
-          hidden = true,
-          ignored = true,
+          hidden = false,
+          ignored = false,
           exclude = { "node_modules", "vendor", ".git", ".next", ".nuxt", ".output", "dist", "build" },
         },
         explorer = {
@@ -33,5 +33,15 @@ return {
   keys = {
     { "<leader>e", function() Snacks.explorer({ layout = { layout = { position = "right" } } }) end, desc = "Explorer (right)" },
     { "<leader>E", function() Snacks.explorer({ layout = { layout = { position = "right" } } }) end, desc = "Explorer (right)" },
+    {
+      "<leader>sG",
+      function()
+        vim.ui.input({ prompt = "Grep in glob: " }, function(glob)
+          if glob and glob ~= "" then Snacks.picker.grep({ glob = glob }) end
+        end)
+      end,
+      desc = "Grep in glob",
+    },
+    { "<leader>fh", function() Snacks.picker.files({ hidden = true, ignored = true }) end, desc = "Find files (hidden + ignored)" },
   },
 }
