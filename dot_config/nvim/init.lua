4bc57a5ca16d3bc8aa09eabcb6dcff57d103f985
Alex Clark <hello@alexclark.co.nz>
2026-07-31 08:56:39 +1200

    chore: update configs

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,12 @@
 vim.g.lazyvim_php_lsp = "intelephense"
 
+vim.lsp.config("laravel_lsp", {
+  cmd = { "laravel-lsp" },
+  filetypes = { "php", "blade" },
+  root_markers = { "artisan", "composer.json", ".git" },
+})
+
+vim.lsp.enable("laravel_lsp")
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
